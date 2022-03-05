011108177b3e42d0998b65276798fd8719d2bd9a
KarlsonAV <andreykartavik@gmail.com>
2022-03-05 18:47:49 +0100

    Init

---
+++ sort/Asorts.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void SS(vector<int> &arr, int n);
+void IS(vector<int> &arr, int n);
+void BS(vector<int> &arr, int n);
+
+int main() {
+    cout << "This program takes as input some numbers and returns sorted list" << endl;
+    int n;
+    cout << "Enter amount of numbers that you would like to sort: ";
+    cin >> n;
+    cout << endl;
+    if (!cin) {
+        cout << "Some error occured" << endl;
+        return 1;
+    }
+    vector<int> arr(n+1);
+    for (int i = 0; i < n; i++) {
+        cout << "Enter element of array: ";
+        cin >> arr[i];
+        cout << endl;
+        if (!cin) {
+            cout << "Some error occured" << endl;
+            return 1;
+        }
+    }
+
+    cout << "Choose sort method: 1 - SS, 2 - IS, 3 - BS, 4 - HS, 5 - MS, 6 - QS, 7 - CS: ";
+    int act;
+    cin >> act;
+    switch(act) {
+        case 1:
+            SS(arr,n);
+            break;
+        case 2:
+            IS(arr,n);
+            break;
+        case 3:
+            BS(arr,n);
+            break;
+        default:
+            break;
+    }
+    for (int i = 0; i < n; i++) {
+        cout << " " << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void SS(vector<int> &arr, int n) {
+    
+    for (int i = 0; i < n - 1; i++) {
+        int min = arr[i];
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < min) {
+                min = arr[j];
+                min_idx = j;
+            }
+        }
+        swap(arr[i], arr[min_idx]);
+    }
+    return;
+}
+
+void IS(vector<int> &arr, int n) {
+
+    for (int i = 1; i < n; i++) {
+        for (int j = i; j > 0; j--) {
+            if (arr[j] < arr[j-1]) {
+                swap(arr[j],arr[j-1]);
+            }
+        }
+    }
+    return;
+}
+
+void BS(vector<int> &arr, int n) {
+
+    for (int i = 0; i < n; i++){
+        for (int j = n - 1; j > i; j--) {
+            if (arr[j] < arr[j-1]) {
+                swap(arr[j], arr[j-1]);
+            }
+        }
+    }
+    return;
+}
