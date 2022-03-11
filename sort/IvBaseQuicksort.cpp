82a5f7028eebbe0abf20954e1a102b9a5175257c
ivankaliadzich <101336193+ivankaliadzich@users.noreply.github.com>
2022-03-11 11:36:37 +0100

    create IvBaseQuicksort.cpp

---
+++ sort/IvBaseQuicksort.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector <int> vec;
+
+void sorting(int l, int r) {
+    int i = l, j = r, mid = (l+r)/2, x;
+    x = vec[mid];
+
+    while ( i <= j) {
+        while (vec[i]<x) {
+            i++;
+        }
+        while (vec[j]>x) {
+                j--;
+        }
+        if (i<=j) {
+            swap(vec[i], vec[j]);
+            i++;
+            j--;
+        }
+    }
+
+    if (i<r){
+        sorting(i, r);
+    }
+
+    if (j>l){
+        sorting(l, j);
+    }
+}
+
+int main(){
+    int l,r,n;
+    cin >> n;
+    vec.resize(n);
+    for (int i=0; i<n; i++){
+        cin >> vec[i];
+    }
+    l=0;
+    r=n-1;
+
+    sorting(l,r);
+
+    for (int i=0; i<n; i++){
+        cout << vec[i] << " ";
+    }
+
+return 0;
+}
